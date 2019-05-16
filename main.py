a858164be7967599502bf7ba622780aaf356accf
jmaloyjr <jmaloyjr@udel.edu>
2019-05-16 17:03:38 -0400

    Added some more ml algorithms

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ import numpy as np
 import pandas as pd
 import urllib 
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+
+# Imports for machine learning algorithms
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
 
 # Process url into comprehensive 2D datatset
 def generateDataSetFromUrl(url):
@@ -31,6 +37,28 @@ def guassianNaiveBayes(dtrain, ttrain, dtest):
     prediction = gnb.fit(dtrain, ttrain).predict(dtest)
     return prediction
 
+# Running training data through a SVC model
+def supportVectorClassification(dtrain, ttrain, dtest):
+
+    svc = LinearSVC(random_state = 10, max_iter = 200000)
+    prediction = svc.fit(dtrain, ttrain).predict(dtest)
+    return prediction
+
+# Running training data through a k neighbors model
+def kNeighbors(dtrain, ttrain, dtest):
+
+    k = KNeighborsClassifier(n_neighbors = 3)
+    k.fit(dtrain, ttrain)
+    prediction = k.predict(dtest)
+    return prediction
+
+# Running training data through decision tree classifier
+def decisionTree(dtrain, ttrain, dtest):
+
+    dTree = DecisionTreeClassifier()
+    dTree = dTree.fit(dtrain, ttrain)
+    prediction = dTree.predict(dtest)
+    return prediction
 
 # Main function declaration
 def main():
@@ -47,8 +75,25 @@ def main():
     data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = 0.2, random_state = 10)
 
     # Running Guassian Naive Bayes
+    print("Running Naive-Bayes....\n")
     gnbPredicition = guassianNaiveBayes(data_train, target_train, data_test)
-    print("Naive-Bayes accuracy : " + str(accuracy_score(target_test, gnbPredicition, normalize = True)))
+    print("Naive-Bayes accuracy : " + str(accuracy_score(target_test, gnbPredicition, normalize = True)) + "\n")
+
+    # Running Support Vector Classification
+    print("Running Support Vector Classification....\n")
+    svcPredicition = supportVectorClassification(data_train, target_train, data_test)
+    print("Support Vector Classification accuracy : " + str(accuracy_score(target_test, svcPredicition, normalize = True)) + "\n")
+
+    # Running k nearest neighbors
+    print("Running K Neighbors Classifier....\n")
+    kNearPredicition = kNeighbors(data_train, target_train, data_test)
+    print("K Neighbor Classifier accuracy : " + str(accuracy_score(target_test, kNearPredicition, normalize = True)) + "\n")
+
+    # Running Decision Tree
+    print("Running Decision Tree Classifier....\n")
+    dTreePredicition = decisionTree(data_train, target_train, data_test)
+    print("Decision Tree Classifier accuracy : " + str(accuracy_score(target_test,dTreePredicition, normalize = True)) + "\n")
+
 
 if __name__ == "__main__":
     main()
